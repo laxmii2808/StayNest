551d37a930902b6e11a91406ce1d2fb0e4a30220
Delta Student <tudulaxmi050@gmail.com>
2025-06-16 23:21:10 +0530

    Update package.json with correct start script

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
